57fc70e5b23f934881add61cd091c5abb0fb3cc6
Conner Hufnagel <conner.huf@gmail.com>
2025-02-06 11:04:16 -0800

    removed auth and deploying current state

--- app/config.py
+++ app/config.py
@@ -10,8 +10,8 @@ class Config:
   SPOTIFY_CLIENT_SECRET: str = os.getenv("SPOTIFY_CLIENT_SECRET")
   TICKETMASTER_API_KEY: str = os.getenv("TICKETMASTER_API_KEY")
   TICKETMASTER_SECRET: str = os.getenv("TICKETMASTER_SECRET")
-  GOOGLE_CLIENT_ID: str = os.getenv("your_google_client_id")
-  GOOGLE_CLIENT_SECRET: str = os.getenv("your_google_client_secret")
+  GOOGLE_CLIENT_ID: str = os.getenv("GOOGLE_CLIENT_ID")
+  GOOGLE_CLIENT_SECRET: str = os.getenv("GOOGLE_CLIENT_SECRET")
   GOOGLE_DISCOVERY_URL: str = os.getenv("https://accounts.google.com/.well-known/openid-configuration")
   SECRET_KEY: str = os.getenv("SECRET_KEY", "strong secret key for jwt")
   
